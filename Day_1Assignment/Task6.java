13e355d7fa73f6c1a08fedcfed6c4334c0e40646
Harshadaghadge-17 <harshadasanjayghadge@gmail.com>
2025-02-18 19:39:13 +0530

    Add files via upload

---
+++ Day_1Assignment/Task6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Task6{
+	public static void main(String[] args){
+	Scanner sc= new Scanner(System.in);
+	int a= sc.nextInt();
+	int b= sc.nextInt();
+	int add= a+b;
+	int sub = a-b;
+	int mult= a*b;
+	int div= a/b;
+	int mod= a%b;	
+	System.out.println(add);
+	
+	System.out.println(sub);
+	
+	System.out.println(mult);
+	
+	System.out.println(div);
+
+	System.out.println(mod);
+}
+}
---
+++ Day_1Assignment/Test.java
@@ -0,0 +1,9 @@
+class Test{
+	public static void main(String args[])
+	{
+		int a=-5+8*6;
+		int b=36;
+		int c=a+b;
+		System.out.println(a );
+	}
+}
